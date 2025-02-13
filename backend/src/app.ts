31a3b0ea36edb333cb3893e4ad4aebf0e58ee9e1
Ron Winchester <rom0203@gmail.com>
2025-02-13 23:27:43 +0300

    feat: typecheck

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,9 +13,6 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello from Express + TypeScript backend!");
-});
 
 app.use(
     "/trpc",
@@ -23,6 +20,10 @@ app.use(
         router: trpcRouter,
     }),
 );
+
+app.get("/", (req, res) => {
+    res.send("Hello from Express + TypeScript backend!");
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
